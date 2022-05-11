3e0fd4a71e3202984ad4fef7314fc0e9071ae335
Ching Lam, Lee <kjha9dsfiu@gmail.com>
2022-05-11 22:56:49 +0300

    unit test code finished but doesn't work

---
+++ Lab4UnitTest/Lab4UnitTest.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab4Part2.3/Options.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab4UnitTest
+{
+	TEST_CLASS(Lab4UnitTest)
+	{
+	public:
+		//Used to test sumOfCol
+
+		TEST_METHOD(TestMethod1)
+		{
+			const int maxRow = 3;
+			const double mat[maxRow][MAX_COL] = { 1,2,3,4,5,6 };
+		// Arrange
+			double total;
+			const int column = 0;
+		// Act
+			
+			total=sumOfCol(mat,column,maxRow);
+		// Assert
+			Assert::AreEqual(9.0, total);
+
+		}
+	};
+}
