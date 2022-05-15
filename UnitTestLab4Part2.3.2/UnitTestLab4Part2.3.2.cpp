983ee9816eb83d3e9c0667dbaf30c336f1f69e9b
Lithiummood <99469250+Lithiummood@users.noreply.github.com>
2022-05-15 19:05:29 +0300

    Update UnitTestLab4Part2.3.2.cpp
    Copy changes from the misplaced unit test file

--- UnitTestLab4Part2.3.2/UnitTestLab4Part2.3.2.cpp
+++ UnitTestLab4Part2.3.2/UnitTestLab4Part2.3.2.cpp
@@ -12,16 +12,17 @@ namespace Lab4UnitTest
 		//Used to test sumOfCol
 		TEST_METHOD(TestMethod_SumOfCol)
 		{
-			// Arrange
 			const int maxRow = 3;
 			const double mat[maxRow][MAX_COL] = { 1,2,3,4,5,6 };
-			double total;
 			const int column = 0;
+		// Arrange
+			double total;
+			
 
-			// Act
-			total = sumOfCol(mat, column, maxRow);
+		// Act
+			total=sumOfCol(mat,column,maxRow);
 
-			// Assert
+		// Assert
 			Assert::AreEqual(9.0, total);
 
 		}
@@ -29,10 +30,10 @@ namespace Lab4UnitTest
 		//Used to test sumOfRow
 		TEST_METHOD(TestMethod_SumOfRow)
 		{
-			// Arrange
 			const int maxRow = 3;
 			const double mat[maxRow][MAX_COL] = { 1,2,3,4,5,6 };
 			const int row = 0;
+			// Arrange
 			double total;
 
 			// Act
@@ -41,22 +42,20 @@ namespace Lab4UnitTest
 			// Assert
 			Assert::AreEqual(3.0, total);
 		}
-
+		
 		//Used to test findMinElement
 		TEST_METHOD(TestMethod_FindMinElement)
 		{
-			// Arrange
-			const double mat[3][2] = { 1,2,3,4,5,6 };
+			const double mat[3][2] = { 1,2,3,4,5,6};
 			const int maxRow = 3;
+			// Arrange
 			double small;
 
 			// Act
 			small = findMinElement(mat, maxRow);
-			//bool isSquare(const double mat[][MAX_COL], const int maxRow) {
-			//findMinElement(const double mat[][MAX_COL], const int maxRow){
 
 			// Assert
-			Assert::AreEqual(1.0, small);
+			Assert::AreEqual(1.0,small);
 		}
 	};
 }
